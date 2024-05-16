6d2d97486ae170ef25469cfe0c2eca6bc3657fc1
rizkyintan <rizkyintan02@mail.ugm.ac.id>
2024-05-17 00:46:38 +0700

    Do the task 1: create parking calculate charge for basic java

--- Tugas1-Basic-Syntax-Challenge/src/main/java/ParkingCalculateApp/calculate.java
+++ Tugas1-Basic-Syntax-Challenge/src/main/java/ParkingCalculateApp/calculate.java
@@ -1,5 +1,29 @@
 package ParkingCalculateApp;
+import java.util.Scanner;
 
 public class calculate {
 
+    public static void main(String[] args){
+
+        // Create variable to store the parking duration
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to SYNRGY Parking Lot 🅿️!");
+        System.out.println("How many hours you've been using our facility Ma'am? ");
+        int duration = scanner.nextInt();
+        scanner.close();
+
+        // Else if method to calculate the parking charge based on the charges duration rules
+        double charge;
+        if (duration <= 5){
+            charge = 1;
+        } else if (duration < 24 ) {
+            charge = 1 + ((duration-5)*0.5);
+        } else {
+            charge = 15 + ((duration-24)*0.5);
+        }
+
+        // Tell customer the total parking charge
+        System.out.println("Great! Your parking charge will be cost $" + charge + " 🪙");
+
+    }
 }
